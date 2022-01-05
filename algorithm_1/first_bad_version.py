c5972f592d8c5d4d5241d7000c47414547e34df4
Mainul Hoque <mdmainulh28@gmail.com>
2022-01-05 16:53:12 +0600

    solved first bad vesrion

--- algorithm_1/first_bad_version.py
+++ algorithm_1/first_bad_version.py
@@ -2,10 +2,19 @@
 # @param version, an integer
 # @return an integer
 # def isBadVersion(version):
-
+    
 class Solution:
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
+        low = 1
+        high = n
+        while low<high:
+            mid = (low+high)//2
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid+1
+        return low
