70c3f653773fa9ab43272832c383e3d166cafb78
Mainul Hoque <mdmainulh28@gmail.com>
2022-01-09 18:07:58 +0600

    modified squares of sorted array

--- algorithm_1/squares_of_sorted_array.py
+++ algorithm_1/squares_of_sorted_array.py
@@ -1,2 +1,12 @@
 def sortedSquares(nums):
-    pass
+    new_list = [-4,-1,0,3,10]
+    negative_list = []
+    for i in new_list:
+        if i<0:
+            negative_list.append(abs(i))
+    
+    print(negative_list)
+    square_list = [i*i for i in new_list]
+    print(sorted(square_list))
+
+sortedSquares(1)
