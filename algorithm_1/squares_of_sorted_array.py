ef2aff2172614f31c6983ba273bff1215687815d
Mainul Hoque <mdmainulh28@gmail.com>
2022-01-13 17:55:50 +0600

    added some logic in squred sorted array

--- algorithm_1/squares_of_sorted_array.py
+++ algorithm_1/squares_of_sorted_array.py
@@ -1,11 +1,14 @@
 def sortedSquares(nums):
     new_list = [-4,-1,0,3,10]
-    negative_list = []
-    for i in new_list:
-        if i<0:
-            negative_list.append(abs(i))
     
-    print(negative_list)
+    p_1 = 0
+    p_2 = len(new_list)-1
+    while p_1<p_2:
+        if abs(new_list[p_1])<abs(new_list[p_2]) and abs(new_list[p_1])>abs(new_list[p_2-1]):
+            new_list.insert(p_2-1, nums[p_1])
+            new_list.pop(p_1)
+            
+    print(new_list)
     square_list = [i*i for i in new_list]
     print(sorted(square_list))
 
