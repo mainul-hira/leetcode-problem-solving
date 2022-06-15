83185ee55ec03fe7a014941c1657346d53e2c090
Mainul Hoque Hira <89728703+mainul-hira@users.noreply.github.com>
2022-06-15 15:28:13 +0600

    Time: 1011 ms (46.93%), Space: 28.7 MB (9.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,7 +3,6 @@ class Solution:
         max_sum, current_sum = -inf , 0
         for i in nums:
             current_sum = max(i, current_sum+i)
-            if max_sum<current_sum:
-                max_sum = current_sum
+            max_sum = max(max_sum, current_sum)
         return max_sum
                 
